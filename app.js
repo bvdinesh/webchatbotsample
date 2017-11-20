b8f2a5c11245c1bcbf6441ab93f93864b8d630b0
Dinesh <dinesh.bhat@cognizant.com>
2017-11-20 15:39:36 -0600

    Updated

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ A simple echo bot for the Microsoft Bot Framework.
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var githubClient = require('./github-client.js');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -19,6 +20,9 @@ var connector = new builder.ChatConnector({
     openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
+
+
+
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
@@ -29,6 +33,44 @@ server.post('/api/messages', connector.listen());
 * ---------------------------------------------------------------------------------------- */
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+var connector = new builder.ChatConnector();
+var bot = new builder.UniversalBot(connector);
+
+var dialog = new builder.IntentDialog();
+dialog.matches(/^search/i, [
+    function (session, args, next) {
+        if (session.message.text.toLowerCase() == 'search') {
+            builder.Prompts.text(session, 'Who are you looking for?');
+        } else {
+            var query = session.message.text.substring(7);
+            next({ response: query });
+        }
+    },
+    function (session, result, next) {
+        var query = result.response;
+        if (!query) {
+            session.endDialog('Request cancelled');
+        } else {
+            githubClient.executeSearch(query, function (profiles) {
+                var totalCount = profiles.total_count;
+                if (totalCount == 0) {
+                    session.endDialog('Sorry, no results found.');
+                } else if (totalCount > 10) {
+                    session.endDialog('More than 10 results were found. Please provide a more restrictive query.');
+                } else {
+                    session.dialogData.property = null;
+                    var usernames = profiles.items.map(function (item) { return item.login });
+                    builder.Prompts.choice(
+                        session,`Please choose a user`,
+                        usernames,
+                        {listStyle : builder.ListStyle.button}
+                    )
+                }
+            });
+        }
+    }, function(session, result, next) {
+        session.send(result.response.entity);
+    }
+]);
+
+bot.dialog('/', dialog);
